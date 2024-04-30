e2005e9799d737d570fa753ca62c7584b56da9aa
Marco Molossi <molossi.marco1906@gmail.com>
2024-05-01 00:38:52 +0200

    Created autocommands.lua and organized code releated to it

--- init.lua
+++ init.lua
@@ -46,20 +46,7 @@ What is Kickstart?
 -- import .lua files
 require 'settings'
 require 'keymaps'
-
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
+require 'autocommands'
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
